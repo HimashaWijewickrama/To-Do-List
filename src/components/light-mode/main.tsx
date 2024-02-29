14bd22f4faf21f3bcf6eaae7f7ead8571d1b753a
myusername <himashawije5678@gmail.com>
2024-02-29 23:25:34 +0530

    delete function changes

--- src/components/light-mode/main.tsx
+++ src/components/light-mode/main.tsx
@@ -18,41 +18,46 @@ import { Header } from "../header";
 export const Main = () => {
   const [inputValue, setInputValue] = useState<string | "">("");
   const [selectValue, setSelectValue] = useState<Date | null>(null);
-  const [tasks, setTasks] = useState<{ task: string; date: Date }[]>([]); // define the type of tasks as string[]
+  const [tasks, setTasks] = useState<{ task: string; date: Date }[]>([]);
   const [show, setShow] = useState(false);
+  const [selectedTask, setSelectedTask] = useState<{
+    task: string;
+    date: Date;
+  } | null>(null);
   const [hide, setHide] = useState(true);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
-  const handleDeleteClick = () => {
-    setHide(false);
+  const handleClose = () => setShow(false);
+  const handleShow = (task: { task: string; date: Date }) => {
+    setSelectedTask(task);
+    setShow(true);
+  };
+
+  const handleDeleteClick = (index: number) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1); // Remove the task at the specified index
+    setTasks(updatedTasks); // Update the tasks state
     Swal.fire({
       icon: "success",
       title: "Done..",
       text: "Your Added Task Removed from the List!",
       footer: '<a href="#">Why do I have this issue?</a>',
-    }).then((result) => {
-      if (result.isConfirmed) {
-        setInputValue("");
-        setSelectValue(null);
-      }
     });
   };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
+
   const handleDateChange = (date: Date | null) => {
-    setSelectValue(date ? date : null); // Update the selected value with the date object or null
+    setSelectValue(date ? date : null);
   };
+
   const handleAddClick = () => {
-    console.log(inputValue);
-    console.log(selectValue);
     if (inputValue.trim() === "" || selectValue === null) {
       Swal.fire({
         icon: "error",
         title: "Oops..",
-        text: "Please Enter a Task Before Adding!",
+        text: "Please Enter a Task Details Before Adding!",
         footer: '<a href="#">Why do I have this issue?</a>',
       });
     } else {
@@ -60,11 +65,9 @@ export const Main = () => {
         task: inputValue,
         date: selectValue,
       };
-      console.log(newTask.date);
       setTasks([...tasks, newTask]);
-      setInputValue(""); // Clear input field after adding task
+      setInputValue("");
       setSelectValue(null);
-      console.log(newTask);
       Swal.fire({
         icon: "success",
         title: "Done..",
@@ -76,10 +79,7 @@ export const Main = () => {
 
   const handleClearClick = () => {
     setInputValue("");
-    console.log(inputValue);
     setSelectValue(null);
-    console.log(selectValue);
-    // (window.location.reload as (cache: boolean) => void)(false);
   };
 
   return (
@@ -89,7 +89,7 @@ export const Main = () => {
 
         <TaskForm
           handleInputChange={handleInputChange}
-          selectValue={selectValue} // Pass selectValue as a prop
+          selectValue={selectValue}
           handleDateChange={handleDateChange}
           handleClearClick={handleClearClick}
           handleAddClick={handleAddClick}
@@ -101,14 +101,14 @@ export const Main = () => {
               <Stack direction="horizontal" gap={1}>
                 <Container>
                   {tasks.map((task, index) => (
-                    <ListGroup as="ul">
+                    <ListGroup as="ul" key={index}>
                       <Row>
                         <Col xs={12} md={10}>
                           <ListGroup.Item
                             as="li"
-                            className="list-group-item list-group-item-action  text-start"
+                            className="list-group-item list-group-item-action text-start"
                           >
-                            <Form.Label key={index} className="fs-6">
+                            <Form.Label className="fs-6">
                               ⚪ {task.task}
                               <br />
                             </Form.Label>
@@ -119,36 +119,18 @@ export const Main = () => {
                           <ButtonGroup className="d-flex mb-2">
                             <Button
                               variant="outline-primary"
-                              onClick={handleShow}
+                              onClick={() => handleShow(task)}
                             >
-                              <GrView /> View{" "}
+                              <GrView /> View
                             </Button>
 
                             <Button
                               variant="outline-danger"
-                              onClick={handleDeleteClick}
+                              onClick={() => handleDeleteClick(index)}
                             >
-                              <GrTrash /> Delete{" "}
+                              <GrTrash /> Delete
                             </Button>
                           </ButtonGroup>
-                          <Modal show={show} onHide={handleClose}>
-                            <Modal.Header closeButton>
-                              <Modal.Title className="fs-6">
-                                View Task
-                              </Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                              Task Description: {task.task}
-                              <br />
-                              Scheduled Date: {task.date.toLocaleDateString()}{" "}
-                              <br />
-                            </Modal.Body>
-                            <Modal.Footer>
-                              <Button variant="secondary" onClick={handleClose}>
-                                Close
-                              </Button>
-                            </Modal.Footer>
-                          </Modal>
                         </Col>
                       </Row>
                     </ListGroup>
@@ -159,6 +141,24 @@ export const Main = () => {
           </div>
         </div>
       </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title className="fs-6">View Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {selectedTask && (
+            <>
+              <p>Task Description: {selectedTask.task}</p>
+              <p>Scheduled Date: {selectedTask.date.toLocaleDateString()}</p>
+            </>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
