8004cf88a46ef8b59c99e0adbd2111f8ee8428bc
myusername <himashawije5678@gmail.com>
2024-03-08 23:27:53 +0530

    changes

--- src/components/light-mode/taskForm.tsx
+++ src/components/light-mode/taskForm.tsx
@@ -1,10 +1,13 @@
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import dayjs from "dayjs";
+import React from "react";
 import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import Stack from "react-bootstrap/Stack";
-import { GrAdd, GrClear } from "react-icons/gr";
-import React from "react";
 import DatePicker from "react-datepicker";
-
+import { GrAdd, GrClear } from "react-icons/gr";
 interface TaskFormProps {
   handleInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   selectValue: Date | null;
@@ -18,7 +21,7 @@ export const TaskForm = (props: TaskFormProps) => {
   return (
     <div className="col-12">
       <div className="p-3 border bg-light">
-        <Stack direction="horizontal" gap={3}>
+        <Stack direction="horizontal" gap={1}>
           <Form.Control
             type="text"
             value={props.inputValue}
@@ -34,6 +37,10 @@ export const TaskForm = (props: TaskFormProps) => {
             placeholderText=" Select date..."
             className="form-control"
           />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <TimePicker className="form-control" />
+          </LocalizationProvider>
+
           <Button variant="secondary" onClick={props.handleAddClick}>
             <GrAdd />
             Add
