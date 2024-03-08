d1c449a4cb0ec846da4566feee2c3bbfc80713ad
myusername <himashawije5678@gmail.com>
2024-03-08 22:49:21 +0530

    changes

--- src/components/dark-mode/header.tsx
+++ src/components/dark-mode/header.tsx
@@ -1,28 +1,32 @@
 import Button from "react-bootstrap/esm/Button";
 import Stack from "react-bootstrap/esm/Stack";
 import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
+import logo from "../../assets/logo.png";
 
 export const Header = () => {
   return (
-    <div className="col-12">
-      <div className="p-3 border bg-dark">
-        <Stack direction="horizontal" gap={3}>
-          <a href="/ToDo-List">
-            <Button
-              variant="light "
-              className="border border-dark rounded-pill fs-6"
-            >
-              <MdOutlineLightMode /> Light Mode
-            </Button>
-          </a>
-          <a href="/ToDo-List/dark-mode">
-            <Button variant="dark" className="rounded-pill fs-6">
-              <MdOutlineDarkMode /> Dark Mode
-            </Button>
-          </a>
-        </Stack>
-        <p className="fw-bold fs-2 text-center text-light">🗓️ MY TO-DO LIST</p>
-      </div>
+    <div className="col-12 p-3 border" id="header-container-dark">
+      <Stack direction="horizontal" gap={3}>
+        <a href="/ToDo-List">
+          <Button
+            variant="light "
+            className="border border-dark rounded-pill fs-6"
+          >
+            <MdOutlineLightMode /> Light Mode
+          </Button>
+        </a>
+        <a href="/ToDo-List/dark-mode">
+          <Button variant="dark" className="rounded-pill fs-6">
+            <MdOutlineDarkMode /> Dark Mode
+          </Button>
+        </a>
+      </Stack>
+
+      <hr className="header-divider" />
+
+      <span className="header-logo-circle">
+        <img src={logo} className="header-logo" alt="My To-Do List" />
+      </span>
     </div>
   );
 };
